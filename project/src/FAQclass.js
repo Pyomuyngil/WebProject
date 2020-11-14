1e4a03aa2e69abe6859b43e2679594f7e56c1525
Pyomuyngil <muyngil@gmail.com>
2020-11-15 01:51:59 +0900

    자게 글쓰기 연결(페이지 안넘어감)

---
+++ project/src/FAQclass.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function FAQclass(){
+  return (
+    <div>
+    <h1> FAQclass </h1>
+    </div>
+  );
+}
+export default FAQclass;
--- project/src/Write.js
+++ project/src/Write.js
@@ -1,115 +1,85 @@
-import React from 'react'
+import React, { Component } from 'react';
 import {Container, Divider, Dropdown, Grid, Header, Image, Button,
   List,  Menu,  Segment} from 'semantic-ui-react'
 
 import Comments from "./comment.js"
+import FreeBoard from "./FreeBoard.js"
 
-const BoardWrite = () => (
-  <div>
-    <Menu size='small' fixed='top' inverted>
-      <Container>
-        <Menu.Item as='a' header>
-          <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
-          같이가요
-        </Menu.Item>
-        <Menu.Item as='a' style={{ marginLeft: '5em' }}>홈</Menu.Item>
-        <Menu.Item as='a'>국내동행찾기</Menu.Item>
-        <Menu.Item as='a'>여행후기</Menu.Item>
-        <Menu.Item as='a'>자유게시판</Menu.Item>
+import {Link,BrowserRouter as Router,Switch,Route,Redirect } from "react-router-dom";
+function BoardWrite(){
 
-        <Dropdown item simple text='문의 및 신고'>
-          <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
-            <Dropdown.Item>
-              <i className='dropdown icon' />
-              <span className='text'>Submenu</span>
-              <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-
-        <Menu.Item>
-          <Button primary>LOGOUT</Button>
-        </Menu.Item>
-
-      </Container>
-    </Menu>
-
+  return (
+    <div>
     <Container text style={{ marginTop: '7em' }}>
-      <Header as='h1'>글쓰기</Header>
-      <div class="ui form">
-        <div class="field">
-          <label>제목</label>
-          <textarea rows="1"></textarea>
-        </div>
-        <div class="field">
-          <label>내용</label>
-          <textarea></textarea>
-        </div>
-      </div>
-      <button class="ui blue button">저장</button>  <button class="ui white button">취소</button>
-      <Image src='/images/wireframe/media-paragraph.png' style={{ marginTop: '2em' }} />
-      <Image src='/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+    <Header as='h1'>글쓰기</Header>
+    <div class="ui form">
+    <div class="field">
+    <label>제목</label>
+    <textarea rows="1"></textarea>
+    </div>
+    <div class="field">
+    <label>내용</label>
+    <textarea></textarea>
+    </div>
+    </div>
+    <Button.Group>
+    <Button positive>Save</Button>
+    <Button.Or />
+    <Button>Cancel</Button>
+    </Button.Group>
     </Container>
 
     <Segment inverted vertical style={{ margin: '5em 0em 0em', padding: '5em 0em' }}>
-      <Container textAlign='center'>
-        <Grid divided inverted stackable>
-          <Grid.Column width={3}>
-            <Header inverted as='h4' content='Group 1' />
-            <List link inverted>
-              <List.Item as='a'>Link One</List.Item>
-              <List.Item as='a'>Link Two</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Header inverted as='h4' content='Group 2' />
-            <List link inverted>
-              <List.Item as='a'>Link One</List.Item>
-              <List.Item as='a'>Link Two</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Header inverted as='h4' content='Group 3' />
-            <List link inverted>
-              <List.Item as='a'>Link One</List.Item>
-              <List.Item as='a'>Link Two</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={7}>
-            <Header inverted as='h4' content='Footer Header' />
-            <p>
-              Extra space for a call to action inside the footer that could help re-engage users.
-            </p>
-          </Grid.Column>
-        </Grid>
+    <Container textAlign='center'>
+    <Grid divided inverted stackable>
+    <Grid.Column width={3}>
+    <Header inverted as='h4' content='Group 1' />
+    <List link inverted>
+    <List.Item as='a'>Link One</List.Item>
+    <List.Item as='a'>Link Two</List.Item>
+    </List>
+    </Grid.Column>
+    <Grid.Column width={3}>
+    <Header inverted as='h4' content='Group 2' />
+    <List link inverted>
+    <List.Item as='a'>Link One</List.Item>
+    <List.Item as='a'>Link Two</List.Item>
+    </List>
+    </Grid.Column>
+    <Grid.Column width={3}>
+    <Header inverted as='h4' content='Group 3' />
+    <List link inverted>
+    <List.Item as='a'>Link One</List.Item>
+    <List.Item as='a'>Link Two</List.Item>
+    </List>
+    </Grid.Column>
+    <Grid.Column width={7}>
+    <Header inverted as='h4' content='Footer Header' />
+    <p>
+    Extra space for a call to action inside the footer that could help re-engage users.
+    </p>
+    </Grid.Column>
+    </Grid>
 
-        <Divider inverted section />
-        <Image centered size='mini' src='/logo.png' />
-        <List horizontal inverted divided link size='small'>
-          <List.Item as='a' href='#'>
-            Site Map
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Contact Us
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Terms and Conditions
-          </List.Item>
-          <List.Item as='a' href='#'>
-            Privacy Policy
-          </List.Item>
-        </List>
-      </Container>
+    <Divider inverted section />
+    <Image centered size='mini' src='/logo.png' />
+    <List horizontal inverted divided link size='small'>
+    <List.Item as='a' href='#'>
+    Site Map
+    </List.Item>
+    <List.Item as='a' href='#'>
+    Contact Us
+    </List.Item>
+    <List.Item as='a' href='#'>
+    Terms and Conditions
+    </List.Item>
+    <List.Item as='a' href='#'>
+    Privacy Policy
+    </List.Item>
+    </List>
+    </Container>
     </Segment>
-  </div>
-)
-
-export default BoardWrite
+    </div>
+  );
+}
+export default BoardWrite;
