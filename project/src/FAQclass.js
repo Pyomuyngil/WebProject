7fe13ffa743d7a14a7491c50754fb8103aeab348
014967 <62296097+014967@users.noreply.github.com>
2020-11-15 14:02:58 +0900

    이것도 overwritten
    명일이꺼랑 합쳐봐야할듯

---
+++ project/src/FAQclass.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+import {Container, Divider, Dropdown, Grid, Header, Image, Button,
+  List,  Menu,  Segment} from 'semantic-ui-react'
+import fire from './config/fire';
+import Comments from "./comment.js";
+import Home from "./Home.js";
+
+class FAQclass extends Component{
+  constructor(props)
+  {
+    super(props);
+    this.state = {user : {}}
+  }
+  render()
+  {
+    return (
+      <div classNmae="newdiv">
+
+<h1>hello~ </h1>
+      </div>
+    )
+  }
+
+
+}
+
+export default FAQclass;
--- project/src/menu.js
+++ project/src/menu.js
@@ -5,25 +5,37 @@ import fire from './config/fire';
 import Comments from "./comment.js";
 import Home from "./Home.js";
 
+import {Link, BroswerRouter as Router,Switch,Route} from "react-router-dom";
+
+function logout(){
+    fire.auth().signOut();
+}
 
 
 
+function menu(){
 
-const FixedMenuLayout = () => (
-
-
+return (
   <div>
+  <nav>
     <Menu size='small' fixed='top' inverted>
       <Container>
         <Menu.Item as='a' header>
           <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
           같이가요
         </Menu.Item>
+        <Link to='/Home'>
         <Menu.Item as='a' style={{ marginLeft: '5em' }}>홈</Menu.Item>
+        </Link>
+        <Link to = '/Find'>
         <Menu.Item as='a'>국내동행찾기</Menu.Item>
+        </Link>
+        <Link to = '/AfterTravle'>
         <Menu.Item as='a'>여행후기</Menu.Item>
+        </Link>
+        <Link to = '/FreeBoard'>
         <Menu.Item as='a'>자유게시판</Menu.Item>
-
+        </Link>
         <Dropdown item simple text='문의 및 신고'>
           <Dropdown.Menu>
             <Dropdown.Item>List Item</Dropdown.Item>
@@ -43,13 +55,13 @@ const FixedMenuLayout = () => (
         </Dropdown>
 
         <Menu.Item>
-          <Button onClick={Home.logout()}>LOGOUT</Button>
+          <Button onClick="logout">LOGOUT</Button>
         </Menu.Item>
 
       </Container>
     </Menu>
-
-    <Container text style={{ marginTop: '7em' }}>
+</nav>
+    <Container>text style={{ marginTop: '7em' }}>
       <Header as='h1'>Semantic UI React Fixed Template</Header>
       <Comments/>
 
@@ -108,6 +120,8 @@ const FixedMenuLayout = () => (
       </Container>
     </Segment>
   </div>
-)
+);
+}
 
-export default FixedMenuLayout
+
+export default menu;
