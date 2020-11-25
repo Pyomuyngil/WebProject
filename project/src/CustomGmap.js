0928574fce76a5b6024fcd80b376e79b1b208956
014967 <62296097+014967@users.noreply.github.com>
2020-11-25 17:29:47 +0900

    저번에 올리지 못한거
    ㅠㅠ

--- project/src/CustomGmap.js
+++ project/src/CustomGmap.js
@@ -3,9 +3,9 @@ import {
   GoogleMap,
   useLoadScript,
   Marker,
-  InfoWindow
-} from '@react-google-maps/api';
+  InfoWindow,
 
+} from '@react-google-maps/api';
 import { Container, Button,
 } from 'semantic-ui-react';
 
@@ -13,6 +13,8 @@ import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
 }from "use-places-autocomplete";
+
+import Geocode from "react-geocode";
 
 import {
   Combobox,
@@ -22,52 +24,90 @@ import {
   ComboboxOption,
 
 }from "@reach/combobox";
-//import {Link,BrowserRouter as Router,Switch,Route,Redirect } from "react-router-dom";
+
 import { formatRelative } from 'date-fns';
 import '@reach/combobox/styles.css';
-import  styles from './mapStyles';
+import styles from './mapStyles';
+Geocode.setApiKey('AIzaSyBSvzkSRE6HON-gbgHmftLBK8SAJZgCe_k');
+Geocode.enableDebug();
 
-
-require('dotenv').config();
-
+const libraries=['places'];
 
 const mapContainerStyle = {
 width : '50vw',
-height : '50vh',
+height : '400px',
 };
-
-
 const options = {
-
+styles : styles,
 disableDefaultUI : true,
 zoomControl : true,
 };
-const libraries=['places'];
+const center ={
+
+lat : 36.621159,
+lng : 127.074172,
+};
+
 
 
 function CustomGmap(){
 
 
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey : 'AIzaSyBSvzkSRE6HON-gbgHmftLBK8SAJZgCe_k',
+    libraries,
+
+  });
 
 
 const [markers, setMarkers] = useState([]);
 const [selected, setSelected] = useState(null);
-const center ={
+const [address2, setAddress2] = useState('');
+const [city, setCity] = useState('');
+const [area , setArea] = useState('');
+const [resultaddress, setResultAddress] = useState([]);
+const [location,setLocation] = useState({
+  location : []
+});
 
-lat : 37.621159,
-lng : 127.074172,
-};
+
+const [tags,setTags] = useState([]);
+const addTags = () => {};
+
+
+function getGeo(lat,lng){
+
+
+  Geocode.fromLatLng(lat,lng).then( response =>
+    {
+      location.location = response.results[0].formatted_address;
+      console.log(location.location);
+      setLocation(location.location);
+
+
+    });
+    console.log(location);
+    return location;
+
+}
+
 
 const onMapClick = React.useCallback((event) => {
 
   setMarkers(current =>
-    [...current, {
-    lat : event.latLng.lat(),
-    lng : event.latLng.lng(),
-    time : new Date(),
-  }
-]);
+    [...current,
+      {
+        lat : event.latLng.lat(),
+        lng : event.latLng.lng(),
+        time : new Date(),
+        location : getGeo(event.latLng.lat(), event.latLng.lng()),
+      }
+    ]
+  );
+
 }, []);
+
+
 const mapRef = React.useRef();
 const onMapLoad = React.useCallback((map) =>
 {
@@ -75,10 +115,6 @@ const onMapLoad = React.useCallback((map) =>
 } , []);
 
 
-  const {isLoaded, loadError} = useLoadScript({
-    googleMapsApiKey : 'AIzaSyBSvzkSRE6HON-gbgHmftLBK8SAJZgCe_k',
-    libraries,
-  });
 
 
 const panTo = React.useCallback(({lat, lng})=>
@@ -92,9 +128,11 @@ const panTo = React.useCallback(({lat, lng})=>
   if(!isLoaded) return "Loading Maps";
 
 
+
+
   return (
     <div>
-      <Container text style={{ marginTop: '5em' }}>
+      <div text style={{ marginTop: '5em'}}>
       <div>
       <h1>
       여행 지역을 지정해주세요
@@ -103,45 +141,73 @@ const panTo = React.useCallback(({lat, lng})=>
       <Locate panTo ={panTo}/>
       <GoogleMap
       mapContainerStyle={mapContainerStyle}
-      zoom={15}
+      zoom={7}
       center={center}
       options = {options}
-      styles = {styles}
-      defaultOoptions={{
-        styles:require('./mapStyles')
-      }}
-      onClick={onMapClick}
+      onClick={onMapClick}  //맵 클릭시 setMarkers에 현재 클릭한것에 대한 정보를 저장
       onLoad = {onMapLoad}
       >
-      {markers.map((markers) => (
+      {
+        markers.map((marker) => (
         <Marker
-        key ={markers.time.toISOString()}
-        position ={{lat : markers.lat , lng : markers.lng}}
-        onClick={ () => {
-          setSelected(markers);
-        }}
+        key ={`${markers.lat}-${marker.lng}`}
+        position ={{lat : marker.lat , lng : marker.lng}}
+        onClick={ () =>
+          {
+          setSelected(marker);
+          console.log(markers);
+          }
+                }
 
          />
-       ))}
+       ))
+      }
 
-       {selected ? (<InfoWindow position= {{lat :selected.lat ,lng : selected.lng}} onCloseClick={() =>
-        { setSelected(null);
+       {
 
-       }}
-       >
-         <div>
-         <h2>선택되었습니다.</h2>
-         <p>선택됨 {formatRelative(selected.time , new Date())}</p>
+         selected ?
+         (
 
-         </div>
 
-         </InfoWindow>) : null}
+
+           <InfoWindow  //해당 마커 클릭시 플로팅되는 작은
+            position= {{lat :selected.lat ,lng : selected.lng}}
+            onCloseClick= {() =>
+              {
+                setSelected(null);
+
+              }}
+
+            >
+           <div>
+           <h2>{location}</h2>
+           <p>선택됨 {formatRelative(selected.time , new Date())}</p>
+
+           </div>
+
+         </InfoWindow>
+       ) : null}
       </GoogleMap>
+    </div>
+
+    </div>
+    <div>
+    <ul>
+    <li>
+    <span>Tag1</span>
+    <i>close</i>
+    </li>
+    <li>
+    <span>Tag2</span>
+    <i>close</i>
+    </li>
+    </ul>
+    </div>
 
     </div>
 
-    </Container>
-    </div>
+
+
 
 );
 }export default CustomGmap;
@@ -149,71 +215,107 @@ const panTo = React.useCallback(({lat, lng})=>
 
 function Locate({panTo})
 {
-  return <button
-  onClick= {() =>{
-    navigator.geolocation.getCurrentPosition(
-      (position) =>{
-    panTo({
-      lat : position.coords.latitude,
-      lng : position.coords.longitude,
-    });
-  },
-  () =>null
-  );
-  }}>locate me!</button>
+  return (
+    <button
+      onClick= {() =>{
+        navigator.geolocation.getCurrentPosition(
+          (position) =>{
+            panTo({
+              lat : position.coords.latitude,
+              lng : position.coords.longitude,
+            });
+          },
+
+          () =>null
+
+        );
+
+      }}>locate me!
+      </button>
+    );
 }
 
 
 
 
-function Search({ panTo})
+function Search({panTo})
 {
-  const {ready, value, suggestions : {status, data}, setValue, clearSuggestions } = usePlacesAutocomplete({
-    requestOptions : {
-      location : {
-        lat : () => 37.621159,
-      lng : () => 127.074172,
-   },
-   radius : 200 * 1000,
+  const {
+    ready,
+    value,
+    suggestions : {status, data},
+    setValue,
+    clearSuggestions
+  } = usePlacesAutocomplete(
+     {
+    requestOptions :
+    {
+        location : { lat : () => 37.621159,  lng : () =>  127.074172 },
+        radius : 200 * 1000,
 
- },
-});
+    },
+    });
+    const handleInput = (e) =>
+    {
+      setValue(e.target.value);
+    };
 
-return (
-<div className="search">
-  <Combobox
-    onSelect = {async (address) =>{
-      setValue(address,  false );
+
+
+
+
+
+
+
+    const handleSelect = async(address) => {
+      setValue(address,false);
       clearSuggestions();
 
       try {
-
-        const result = await getGeocode({address});
-        const { lat , lng} = await getLatLng(result[0]);
+        const results = await getGeocode({address});
+        const {lat , lng} = await getLatLng(results[0]);
         panTo({lat,lng});
 
-  }catch(error){
-    console.log("error");
-  }
-} }>
-<ComboboxInput value ={value} onChange={(e) =>
-{
-  setValue(e.target.value);
-}}
-disable={!ready}
-placeholder="Enter an address"
-/>
-<ComboboxPopover>
-<ComboboxList>
-{status === "OK" && data.map(({id,  description}) =>
-(<ComboboxOption key ={id} value = {description} />
-))}
-</ComboboxList>
-</ComboboxPopover>
 
-</Combobox>
-</div>
-)
+         const address2 =results[0].formatted_address,
+              address2Array = results[0].address_components;
+
+              console.log(address2);
+
+
+      }catch (error) {
+        console.log("Error : " , error);
+      }
+
+
+    };
+
+
+
+    return (
+        <div className="search">
+          <Combobox onSelect={handleSelect}>
+          <ComboboxInput
+          value ={value}
+          onChange={handleInput}
+          disabled={!ready}
+          placeholder="Enter an address"
+          />
+          <ComboboxPopover>
+            <ComboboxList>
+            {status === "OK" &&
+              data.map(({id,  description}) =>(
+                <ComboboxOption
+                key ={id}
+                value = {description} />
+            ))}
+            </ComboboxList>
+            </ComboboxPopover>
+
+            </Combobox>
+       </div>
+);
+
 
 
 }
