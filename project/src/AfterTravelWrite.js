36e25b53450c0febd893b95ab9b1267017891aa4
Jamey Ryu <jamey0320@naver.com>
2020-11-29 22:07:50 +0900

    Update AfterTravelWrite.js

--- project/src/AfterTravelWrite.js
+++ project/src/AfterTravelWrite.js
@@ -8,6 +8,49 @@ import { Link,BrowserRouter as Router,Switch,Route,Redirect, useHistory  } from 
 function AfterTravelWrite(){
 
 
+const history = useHistory();
+var [titletext, setTitletext ] = useState("");
+var [contentstext , setContentstext] = useState("");
+
+const titleHandler = (e) =>
+{
+   e.preventDefault();
+  setTitletext(e.target.value);
+
+}
+const contentsHandler = (e) =>
+{
+   e.preventDefault();
+    setContentstext(e.target.value);
+}
+
+const saveData = (e)  =>
+{
+ e.preventDefault();
+
+  if(titletext === '' || contentstext ==='')
+  {
+
+  }
+  else {
+    var title = titletext;
+    var contents = contentstext;
+    var writeDate = Date.now();
+    var userId = fire.auth().currentUser;
+    fire.database().ref('여행후기').push().set({
+      userId : userId.uid,
+      email : userId.email,
+      title : title,
+      contents : contents,
+      writeDate : writeDate
+    });
+
+    setTitletext('');
+    setContentstext('');
+    history.goBack();
+  }
+}
+
 
   return (
     <form>
@@ -18,15 +61,15 @@ function AfterTravelWrite(){
     <div class ='ui form'>
     <div class="field">
     <label>제목</label>
-    <Input fluid id = "titlearea" ></Input>
+    <Input fluid id = "titlearea" onChange ={titleHandler}></Input>
     </div>
     <div class="field">
     <label>내용</label>
-    <textarea id ="contentsarea" rows='20'></textarea>
+    <textarea id ="contentsarea" rows='20' onChange = {contentsHandler}></textarea>
     </div>
     </div>
     <Button.Group>
-    <Button positive>Save</Button>
+    <Button onClick={saveData} positive>Save</Button>
     <Button.Or />
     <Link to='/AfterTravel'>
     <Button>Cancel</Button>
@@ -37,6 +80,7 @@ function AfterTravelWrite(){
 
     </div>
     </form>
+
   );
 }
 export default AfterTravelWrite;
