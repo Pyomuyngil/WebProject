df842b66bede45ddda2f3c233df724e588cde1b0
Pyomuyngil <muyngil@gmail.com>
2020-12-05 00:42:21 +0900

    홈화면 최신글 목록 생성

--- project/src/Homepage.js
+++ project/src/Homepage.js
@@ -1,10 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component ,useState } from 'react';
 import {Card, Icon, Container, Divider, Dropdown, Grid, Header, Image, Button,
   List,  Menu,  Segment, Pagination, Table} from 'semantic-ui-react'
 import {Link,BrowserRouter as Router,Switch,Route,Redirect } from "react-router-dom";
 import BoardWrite from "./BoardWrite.js"
-
+import fire from './config/fire';
 function Homepage(){
+
+  const [freeboard , setFreeBoard] = useState([]);
+  const [aftertravel, setAfterTravel] = useState([]);
+  React.useEffect(() =>{
+
+
+  var userId = fire.auth().currentUser;
+
+  var query = fire.database().ref('자유게시판').limitToLast(5);
+
+  const loadingFreeBoard = query.on("value" , snapshot =>
+      {
+        snapshot.forEach(function(childSnapshot)
+        {
+          setFreeBoard(data =>
+            [...data,
+              {
+              title : childSnapshot.val().title,
+              contents : childSnapshot.val().contents,
+              writeDate : childSnapshot.val().writeDate,
+              email :childSnapshot.val().email,
+              userId : childSnapshot.val().userId,
+              }
+            ]);
+        }
+      );
+       }
+     );
+
+    return () => {
+          query.off('value', loadingFreeBoard);
+
+
+        };
+  },[]);
+
+  React.useEffect(() =>{
+
+
+  var userId = fire.auth().currentUser;
+
+  var query = fire.database().ref('여행후기').limitToLast(5);
+
+  const loadingAfterTravel = query.on("value" , snapshot =>
+      {
+        snapshot.forEach(function(childSnapshot)
+        {
+          setAfterTravel(data =>
+            [...data,
+              {
+              title : childSnapshot.val().title,
+              contents : childSnapshot.val().contents,
+              writeDate : childSnapshot.val().writeDate,
+              email :childSnapshot.val().email,
+              userId : childSnapshot.val().userId,
+              }
+            ]);
+        }
+      );
+       }
+     );
+
+    return () => {
+          query.off('value', loadingAfterTravel);
+
+
+        };
+  },[]);
+
+
+
   return (
 <Router>
 <div class ='ui form' style={{ marginTop: '8em', marginLeft: '2em' }}>
@@ -31,35 +102,27 @@ function Homepage(){
   <Header as='h2'>국내동행찾기 최근 글</Header>
   <Table celled selectable>
       <Table.Body>
+      {freeboard.map((item) =>{
+        return(
         <Table.Row>
-          <Table.Cell>
-          <div>'abcd'님 어서오세요!</div>
-          <div><a>회원</a></div>
-          </Table.Cell>
+          <Table.Cell>{item.title}</Table.Cell>
+          <Table.Cell>{item.writeDate}</Table.Cell>
         </Table.Row>
-        <Table.Row>
-          <Table.Cell>Jamie</Table.Cell>
-        </Table.Row>
-        <Table.Row>
-          <Table.Cell>Jill</Table.Cell>
-        </Table.Row>
+      )
+    })}
       </Table.Body>
     </Table>
     <Header as='h2'>여행후기 최근 글</Header>
     <Table celled selectable>
         <Table.Body>
-          <Table.Row>
-            <Table.Cell>
-            <div>'abcd'님 어서오세요!</div>
-            <div><a>회원</a></div>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Jamie</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Jill</Table.Cell>
-          </Table.Row>
+          {aftertravel.map((item) =>{
+            return(
+            <Table.Row>
+              <Table.Cell>{item.title}</Table.Cell>
+              <Table.Cell>{item.writeDate}</Table.Cell>
+            </Table.Row>
+          )
+        })}
         </Table.Body>
       </Table>
   </Grid.Column>
