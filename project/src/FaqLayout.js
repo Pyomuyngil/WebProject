5ad13d0fd5872efaee7c856a644ce8c857248352
Pyomuyngil <muyngil@gmail.com>
2020-12-15 23:43:13 +0900

    Update FaqLayout.js

--- project/src/FaqLayout.js
+++ project/src/FaqLayout.js
@@ -17,15 +17,11 @@ const FaqLayout = () => (
         <tbody>
           <tr>
             <td>1</td>
-            <Link to='/FaqOne'>
-            <td>'같이가요'는 어떤 사이트인가요?</td>
-            </Link>
+            <td><Link to='/FaqOne'>'같이가요'는 어떤 사이트인가요?</Link></td>
           </tr>
           <tr>
             <td>2</td>
-            <Link to='/FaqTwo'>
-            <td>홈페이지 이용방법은 어떻게 되나요?</td>
-            </Link>
+            <td><Link to='/FaqTwo'>홈페이지 이용방법은 어떻게 되나요?</Link></td>
           </tr>
         </tbody>
       </table>
